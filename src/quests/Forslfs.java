7e911baed82fd90be07a910afed08f7fd7c1ef8f
scvgong <gongmyeongyun@gmail.com>
2024-03-25 04:34:58 +0000

    add

--- src/quests/Forslfs.java
+++ src/quests/Forslfs.java
@@ -5,12 +5,19 @@ import java.util.Scanner;
 public class Forslfs {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int result = sc.nextInt();
+        int number = sc.nextInt();
         
-        for(int i = 1 ; i <= result ; i++ ){
-            System.out.printf("Math.pow(%d, %d) => %f\n" , 2, i, Math.pow(2, i));
+        for(int i = 1 ; i <= number ; i++ ){
+            int result = (int)Math.pow(2, i);
+
+            if(result % 8 == 0){
+                System.out.println("2** " + i + "= " + result );
+            }
+            else{
+                System.out.println("8의 배수 아님");
+            }
                         
         }
-
+        sc.close();
     }
 }
